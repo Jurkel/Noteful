e46088c8f333b4d0c5b9dc0fe666b2ab95e074e8
Harvey Le <harveyxcle@gmail.com>
2020-04-08 18:15:35 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,105 @@
-import React from 'react';
+import React from 'react'
 import { Route, Link } from 'react-router-dom'
+import NoteListNav from './NoteListNav/NoteListNav'
+import NoteListMain from './NoteListMain/NoteListMain'
+import NotePageMain from './NotePageMain/NotePageMain'
+import NotePageNav from './NotePageNav/NotePageNav'
+import dummyStore from './dummy-store'
+import {getNotesForFolder, findNote, findFolder} from './note-helpers'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <Link to='/'>Noteful</Link>
-      </header>
-    </div>
+class App extends React.Component {
+  state = {
+    notes: [],
+    folders: []
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState(dummyStore), 600)
+  }
+
+  renderNavRoutes() {
+    const {notes, folders} = this.state;
+    return (
+      <>
+        {['/', '/folder/:folderId'].map(path => (
+          <Route
+            exact
+            key={path}
+            path={path}
+            render={routeProps => (
+              <NoteListNav
+                folders={folders}
+                notes={notes}
+                {...routeProps}
+              />
+            )}
+          />
+        ))}
+        <Route
+          path='/note/:noteId'
+          render={routeProps => {
+            const {noteId} = routeProps.match.params
+            const note = findNote(notes, noteId) || {}
+            const folder = findFolder(folders, note.folderId)
+            return <NotePageNav {...routeProps} folder={folder} />
+          }}
+         /> 
+      </>
+    )
+  }
+
+  renderMainRoutes() {
+    const {notes} = this.state
+    return (
+      <>
+        {['/', '/folder/folderId'].map(path => (
+          <Route
+            exact
+            key={path}
+            path={path}
+            render={routeProps => {
+              const {folderId} = routeProps.match.params
+              const notesForFolder = getNotesForFolder(
+                notes,
+                folderId
+              )
+              return (
+                <NoteListMain 
+                  {...routeProps}
+                  notes={notesForFolder}
+                />
+              )
+            }}
+          />
+        ))}
+        <Route 
+          path='/note/:noteId'
+          render={routeProps => {
+            const {noteId} = routeProps.match.params
+            const note = findNote(notes, noteId)
+            return <NotePageMain {...routeProps} note={note} />
+          }}
+        />
+      </>
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <nav className='App_nav'>{this.renderNavRoutes}</nav>
+        <header className='App_header'>
+          <h1>
+            <Link to='/'>Noteful</Link>{' '}
+          </h1>
+        </header>
+        <main className='App_main'>{this.renderMainRoutes}</main>
+        
+      </div>
   );
+  }
+  
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faPlus, faChevronLeft, faTrashAlt, faCheckDouble
+} from '@fortawesome/free-solid-svg-icons'
+import App from './App'
+
+library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    div
+  )
+  ReactDOM.unmountComponentAtNode(div)
+})
