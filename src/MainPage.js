0bea06d284ee32096b7cdf292f20f8a8507fef08
Harvey Le <harveyxcle@gmail.com>
2020-04-08 11:50:31 -0400

    initial commit

---
+++ src/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import FolderPage from './FolderPage'
+import NotePage from './NotePage'
+
+function MainPage() {
+  return(
+    <div>
+      
+    </div>
+  )
+}
+
+export default MainPage
